7d8ddcc1a05da4730de1a3a1f884eb0d65d8c5c4
NoriSte <nori.ste.magni@gmail.com>
2019-03-25 18:18:37 +0100

    test: 💍 Test the proto file save

--- cypress/integration/cypress-protobuf.test.js
+++ cypress/integration/cypress-protobuf.test.js
@@ -20,20 +20,58 @@ context('Tests', () => {
         // load the same .proto file used by the client
         protoFilePath: "./public/example.proto",
       })
-    .then(encodedJson => {
-      // then create a route with the encoded fixture
-      cy.route({
-        headers: {
-          "content-type": "application/octet-stream"
-        },
-        response: encodedJson,
-        url: '/protobuf-message'
-      }).as("example");
-    });
+      .then(encodedJson => {
+        // then create a route with the encoded fixture
+        cy.route({
+          headers: {
+            "content-type": "application/octet-stream"
+          },
+          response: encodedJson,
+          url: '/protobuf-message'
+        }).as("example");
+      });
 
-    cy.get("button").click();
-    cy.wait("@example");
-    cy.get("#response").contains(`"Message": "Hello from the Cypress Protobuf plugin"`);
-  });
+      cy.get("button").click();
+      cy.wait("@example");
+      cy.get("#response").contains(`"Message": "Hello from the Cypress Protobuf plugin"`);
+    });
+  })
+
+  it('The fixture encoding should work with a previously read proto file', () => {
+    cy.task("protobufEncode", {
+      protoFilePath: "./public/example.proto",
+    })
+
+    // go to the baseUrl page (see cypress.json)
+    cy.visit('/')
+
+    // start the server to allow requests interception
+    cy.server();
+
+    // read the fixture
+    cy.fixture("example.json")
+    .then(json => {
+      // then encode it
+      cy.task("protobufEncode", {
+        // pass the fixture content
+        fixtureBody: json,
+        // specify the protobuf message name
+        message: "Example"
+      })
+      .then(encodedJson => {
+        // then create a route with the encoded fixture
+        cy.route({
+          headers: {
+            "content-type": "application/octet-stream"
+          },
+          response: encodedJson,
+          url: '/protobuf-message'
+        }).as("example");
+      });
+
+      cy.get("button").click();
+      cy.wait("@example");
+      cy.get("#response").contains(`"Message": "Hello from the Cypress Protobuf plugin"`);
+    });
   })
 })
